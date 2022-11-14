d10f7fb34a9358d4b81f0081feaf4c1c7b7f19ff
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2022-11-14 11:14:46 -0300

    abstraindo o container

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -13,10 +13,6 @@ import { makeStyles } from '@material-ui/core';
 import TemplateDefault from '../../src/templates/Default';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(8,0,6)
-  },
-
   buttonAdd: {
     margin: '30px auto',
     display: 'block'
@@ -31,7 +27,7 @@ export default function Home() {
   const classes = useStyles()
   return (
     <TemplateDefault>
-      <Container maxWidth="sm" className = {classes.container}>
+      <Container maxWidth="sm">
         <Typography component='h1' variant="h2" align = 'center'>
           Meus Anúncios
         </Typography>
